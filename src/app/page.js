6534651ae70b8a5d29320308a7bf29110e23fd78
ibrahim_glmz <gulhezibrahim@gmail.com>
2025-07-20 03:59:50 +0300

    video kırpıldı

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function HomePage() {
         {/* Arka Plan Video */}
         <video
           className="absolute top-0 left-0 w-full h-full object-cover z-0"
-          src="/drone1.mp4"
+          src="/drone1_parca1.mp4"
           autoPlay
           loop
           muted
